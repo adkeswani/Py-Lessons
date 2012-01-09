d4b15374f083363d38f7a1d13b40554a37bf4c21
Aditya Keswani <adkeswani@gmail.com>
2012-01-09 14:07:21 +1100

    Finalising 0 and 1

---
+++ python/temp/a.py
@@ -0,0 +1,8 @@
+class bollocks(object):
+    def __init__(self, blah):
+        print "Other init"
+
+    def blah(self):
+        print "Blah"
+
+b = bollocks("a")
---
+++ python/temp/b.py
@@ -0,0 +1,10 @@
+import a
+
+class A(object):
+    def __init__(self):
+        self.b = a.bollocks()
+        print "Blah"
+        self.b.DoStuff()
+
+A()
+
